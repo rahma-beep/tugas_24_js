dcf7aee831d99fb64b33ef751dd1e8c8fe42bcdd
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-17 21:30:16 +0800

    TUGAS 24 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+  var deret = [32,33,16,40,9,7,11,2,44,1,66];
+  console.log("Sebelumnya : " + deret);
+
+  var ascending = deret.sort();
+  console.log("Ascending : " + ascending);
+
+  var descending = deret.reverse();
+  console.log("Descending : " + descending);
